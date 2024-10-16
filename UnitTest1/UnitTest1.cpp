160e5b68f4b2b9daf6843c1b7d86e7e7de14f5d7
Yura <urijkisil2007@gmail.com>
2024-10-16 16:13:07 +0300

    f master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,68 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_7_1_rek/lab_7_1_rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestCreate)
+        {
+            const int rowCount = 3;
+            const int colCount = 3;
+            const int Low = 1;
+            const int High = 10;
+            int** R = new int* [rowCount];
+
+            for (int i = 0; i < rowCount; i++)
+                R[i] = new int[colCount];
+
+            Create(R, rowCount, colCount, Low, High);
+
+            // Перевірка значень в матриці
+            for (int i = 0; i < rowCount; i++) {
+                for (int j = 0; j < colCount; j++) {
+                    Assert::IsTrue(R[i][j] >= Low && R[i][j] <= High);
+                }
+            }
+
+            for (int i = 0; i < rowCount; i++)
+                delete[] R[i];
+            delete[] R;
+        }
+
+        TEST_METHOD(TestCalc)
+        {
+            const int rowCount = 3;
+            const int colCount = 3;
+            int** R = new int* [rowCount];
+            for (int i = 0; i < rowCount; i++)
+                R[i] = new int[colCount];
+
+            // Встановимо значення матриці
+            R[0][0] = 2; R[0][1] = 4; R[0][2] = 6;
+            R[1][0] = 1; R[1][1] = 5; R[1][2] = 3;
+            R[2][0] = 8; R[2][1] = 10; R[2][2] = 12;
+
+            // Сортуємо масив перед підрахунком
+            Sort(R, rowCount, colCount);
+
+            int S = 0, k = 0;
+            Calc(R, rowCount, colCount, S, k);
+
+            // сума парних чисел: 2 + 4 + 6 + 8 + 10 + 12 = 42
+            Assert::AreEqual(42, S);
+            // Кількість парних чисел = 6
+            Assert::AreEqual(6, k);
+
+            for (int i = 0; i < rowCount; i++)
+                delete[] R[i];
+            delete[] R;
+        }
+
+	};
+}
---
+++ lab_7_1_rek/lab_7_1_rek.cpp
@@ -0,0 +1,107 @@
+﻿// lab_7_1_rek.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <iomanip>
+#include <ctime> 
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High, int i = 0, int j = 0);
+void Print(int** a, const int rowCount, const int colCount, int i = 0);
+void Sort(int** R, const int rowCount, const int colCount, int i0 = 0);
+void Change(int** R, const int row1, const int row2, const int colCount, int col = 0);
+void Calc(int** R, const int rowCount, const int colCount, int& S, int& k, int row = 0, int col = 0);
+
+int main() {
+    srand(static_cast<unsigned>(time(NULL)));
+    int Low = 4;
+    int High = 51;
+    int rowCount = 8;
+    int colCount = 5;
+
+    int** R = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        R[i] = new int[colCount];
+
+    Create(R, rowCount, colCount, Low, High);
+    cout << "Generated Matrix:" << endl;
+    Print(R, rowCount, colCount);
+    cout << endl;
+
+    Sort(R, rowCount, colCount);
+    Print(R, rowCount, colCount);
+    cout << endl;
+
+    int S = 0;
+    int k = 0;
+    Calc(R, rowCount, colCount, S, k);
+    cout << "S = " << S << endl;
+    cout << "k = " << k << endl;
+    cout << endl;
+
+    Print(R, rowCount, colCount);
+
+    for (int i = 0; i < rowCount; i++)
+        delete[] R[i];
+    delete[] R;
+
+    return 0;
+}
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High, int i, int j) {
+    a[i][j] = Low + rand() % (High - Low + 1);
+    if (j < colCount - 1)
+        Create(a, rowCount, colCount, Low, High, i, j + 1);
+    else if (i < rowCount - 1)
+        Create(a, rowCount, colCount, Low, High, i + 1, 0);
+}
+
+void Print(int** a, const int rowCount, const int colCount, int i) {
+    if (i < rowCount) {
+        for (int j = 0; j < colCount; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+        Print(a, rowCount, colCount, i + 1);
+    }
+}
+
+void Sort(int** R, const int rowCount, const int colCount, int i0) {
+    if (i0 >= rowCount - 1) return; // Вихід, якщо досягли кінця
+    for (int i1 = 0; i1 < rowCount - i0 - 1; i1++) {
+
+        if ((R[i1][0] > R[i1 + 1][0]) ||
+            (R[i1][0] == R[i1 + 1][0] && R[i1][1] < R[i1 + 1][1]) ||
+            (R[i1][0] == R[i1 + 1][0] && R[i1][1] == R[i1 + 1][1] && R[i1][2] < R[i1 + 1][2])) {
+            Change(R, i1, i1 + 1, colCount);
+        }
+    }
+    Sort(R, rowCount, colCount, i0 + 1); // Переход до наступного раунду сортування
+}
+
+void Change(int** R, const int row1, const int row2, const int colCount, int col) {
+    if (col >= colCount) return; // Вихід, якщо всі стовпці оброблені
+
+
+    int tmp = R[row1][col];
+    R[row1][col] = R[row2][col];
+    R[row2][col] = tmp;
+
+
+    Change(R, row1, row2, colCount, col + 1);
+}
+
+void Calc(int** R, const int rowCount, const int colCount, int& S, int& k, int row, int col) {
+    if (row >= rowCount) return; // Вихід, якщо досягли кінця
+    if (col >= colCount) {
+        Calc(R, rowCount, colCount, S, k, row + 1, 0); // Переход до наступного рядка
+        return;
+    }
+
+    if (!(R[row][col] % 2 != 0 || (row % 3 == 0 && col % 3 == 0))) {
+        S += R[row][col];
+        k++;
+        R[row][col] = 0;
+    }
+    Calc(R, rowCount, colCount, S, k, row, col + 1); // Переход до наступного стовпця
+}
